f8da90feacc3d643cc3b1c8824369ad1818704f0
Ayush bahuguna <ayushbahuguna1122@gmail.com>
2024-03-22 15:38:11 +0000

    dark mode using tailwindcss

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ import Darkmode from '@/Components/Dark mode/Darkmode'
 export default function page() {
   return (
     <div className='dark:bg-black dark:text-white'>
-      <div className='flex items-center justify-center my-10 gap-5'>
+      <div className='flex items-center justify-center py-7 gap-5'>
         <p>Switch modes (Dark/light)</p>
         <Darkmode />
       </div>
